6a17aa0ff48b064666d3fb32012b9d554c1abb83
Adam Solove <asolove@gmail.com>
2024-01-15 11:05:59 -0500

    Basic monadic parser combinator.
    Importing this from my actual AoC work, where I did the basic
    implementation and added a bunch of useful helpers.

--- backtrack.ts
+++ backtrack.ts
@@ -1,172 +1,130 @@
-// Reminding myself how to build a backtracking monad
-// to solve 8 queens.
-function countNQueens(n) {
+// A tiny monadic parser for quickly doing AoC problems
+//
+// A big thanks to Kris for [TDPTSNBN](https://www.youtube.com/watch?v=Q1YXEqCl650)
+// And to Hutton and Meijer for [Monadic Parser Combinators](https://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf)
 
-    function pickCol(next) {
-        for(let i=0; i<n; i++) {
-            let r = next(i);
-            if (r.type==='success') {
-                return r;
-            }
-        }
-        return {type: 'error', message: 'ran out of values'};
-    }
-    
-    function checkCol(val, n) {
-        for(let i=0; i<val.length; i++) {
-            if(val[i] === n) 
-                return false;
-            if(Math.abs(val[i] - n) === val.length - i)
-                return false;
-        }
-        return true;
-    }
-    
-    
-    function guard(test, next) {
-        if(test()) {
-            return next();
-        } else {
-            return {type: 'error', message: 'failed guard'};
-        }
-    }
+export type PartialParse<A> = { value: A; rest: string };
+export type Parser<A> = (input: string) => Iterable<PartialParse<A>>;
 
-
-    let r = 0; 
-    function done(value) {
-        r++;
-        return {type: 'error', message: `Found ${r}: find another.`};
-    }
-    
-    let next = done;
-    for(let i=0; i<n; i++) {
-        let oldNext = next;
-        next = (val) => {
-            return pickCol((n) => {
-                return guard(() => checkCol(val, n), () => {
-                    return oldNext([...val, n])
-                });
-            });
-        };
-    }
-    
-    console.log(next([]));
-    return r;
+export function parse<A>(parser: Parser<A>, input: string): A {
+  for (const result of parser(input)) {
+    return result.value;
+  }
+  throw new Error("no results for parse");
 }
 
-
-// Ok so that works but it forces us to write the whole thing
-// as a stack of callbacks to share state, which is unpleasant
-// for a parser DSL.
-// 
-// Can we find a way to sugar this up so we have nice combinators
-// that don't have be nested scopes to combine data?
-
-/*
-    digit = char('1') -> (c) => (s, next) => if s = c then next(i+1) else error
-    int = oneOrMore(digit) -> (p) => (s, next) => 
-    float = seq(oneOrMore(digit), char('.'), oneOrMore(digit))
-    program = seq(float, EOF)
-
-    seq(a, b) -> (next) => a((v1) => b((v2 => next())))
-
-    base language:
-    char('a') -> 'a' or error
-    or a, b -> needs backtracking
-    seq -> needs backtracking
-
-    sugar:
-    a+ -> seq(a, a*)
-    a* -> or(a, next)
-*/
-
-type PartialParse<A> = {value: A, rest: string};
-type Parser<A> = (input: string) => Iterable<PartialParse<A>>
-
-function result<A>(value: A): Parser<A> {
-    return function*(input: string) {
-        yield {value, rest: input};
-    }
+export function result<A>(value: A): Parser<A> {
+  return function* (input: string) {
+    yield { value, rest: input };
+  };
 }
 
-const zero: Parser<never> = (_input: string) => {
-    return [][Symbol.iterator]();
+export const zero: Parser<never> = (_input: string) => {
+  return [][Symbol.iterator]();
 };
 
-function* item(input: string): Iterable<PartialParse<string>> {
-    if(input[0]) {
-        yield {value: input[0], rest: input.slice(1)};
+export function* item(input: string): Iterable<PartialParse<string>> {
+  if (input[0]) {
+    yield { value: input[0], rest: input.slice(1) };
+  } else {
+    yield* zero(input);
+  }
+}
+
+export function bind<A, B>(
+  p1: Parser<A>,
+  next: (a: A) => Parser<B>
+): Parser<B> {
+  return function* (input: string) {
+    for (const { value, rest } of p1(input)) {
+      yield* next(value)(rest);
+    }
+  };
+}
+
+export function map<A, B>(p1: Parser<A>, transform: (a: A) => B): Parser<B> {
+  return bind(p1, (a) => result(transform(a)));
+}
+
+export function seq<A, B>(p1: Parser<A>, p2: Parser<B>): Parser<[A, B]> {
+  return bind(p1, (a) => bind(p2, (b) => result([a, b])));
+}
+
+export function sat(test: (string) => boolean): Parser<string> {
+  return bind(item, (ch: string) => {
+    if (test(ch)) {
+      return result(ch);
     } else {
-        yield* zero(input);
+      return zero;
     }
+  });
 }
 
-function bind<A, B>(p1: Parser<A>, next: (a: A) => Parser<B>): Parser<B> {
-    return function*(input: string) {
-        for(const {value, rest} of p1(input)) {
-            yield* next(value)(rest);
-        }
+export function char<A extends string>(ch: A): Parser<A> {
+  return sat(function (s: string): s is A {
+    return s === ch;
+  }) as Parser<A>; // FIXME: can we thread the type assertion through sat and bind?
+}
+export const digit = sat((x) => x >= "0" && x <= "9");
+export const upper = sat((x) => x >= "A" && x <= "Z");
+export const lower = sat((x) => x >= "a" && x <= "z");
+export const alpha = plus(upper, lower);
+export const alphanumeric = plus(alpha, digit);
+export const int: Parser<number> = bind(many1(digit), (chars: string[]) =>
+  result(parseInt(chars.join(""), 10))
+);
+export const name = many1(alpha);
+
+export function string<A extends string>(st: A): Parser<A> {
+  return function* (input: string) {
+    if (input.slice(0, st.length) === st) {
+      yield { value: st, rest: input.slice(st.length) };
+    } else {
+      return zero;
     }
+  };
 }
 
-function seq<A, B>(p1: Parser<A>, p2: Parser<B>): Parser<[A, B]> {
-    return bind(p1, (a) => bind(p2, (b) => result([a, b])));
+export function many<A>(p: Parser<A>): Parser<A[]> {
+  return plus(
+    bind(p, (r1) => {
+      return bind(many(p), (rs) => {
+        return result([r1, ...rs]);
+      });
+    }),
+    result([])
+  );
 }
 
-function sat(test: (string) => boolean): Parser<string> {
-    return bind(item, (ch: string) => {
-        if(test(ch)) {
-            return result(ch);
-        } else {
-            return zero;
-        }
-    })
-}
-const char = (ch: string) => sat(x => x === ch);
-const digit = sat(x => x >= '0' && x <= '9');
-const upper = sat(x => x >= 'A' && x <= 'Z' );
-const lower = sat(x => x >= 'a' && x <= 'z');
-const alpha = plus(upper, lower);
+export const line = map(seq(many(sat((c) => c != "\n")), char("\n")), (r) =>
+  [...r[0], r[1]].join("")
+);
 
-const string: (string) => Parser<string> = (st: string) => {
-    return function*(input: string) {
-        if(input.slice(0, st.length) === st) {
-            yield {value: st, rest: input.slice(st.length)}
-        }  else {
-            return zero;
-        }
-    }
-}
-
-function many<A>(p: Parser<A>): Parser<A[]> {
-    return plus(bind(p, (r1) => {
-        return bind(many(p), (rs) => {
-            return result([r1, ...rs]);
-        });
-    }), result([]));
-}
-
-function many1<A>(p: Parser<A>): Parser<A[]> {
-    return bind(p, (r1) => {
-        return bind(many(p), (rs) => {
-            return result([r1, ...rs]);
-        });
+export function many1<A>(p: Parser<A>): Parser<A[]> {
+  return bind(p, (r1) => {
+    return bind(many(p), (rs) => {
+      return result([r1, ...rs]);
     });
+  });
 }
 
-function plus<A>(p1: Parser<A>, p2: Parser<A>): Parser<A> {
-    return function* (input: string) {
-        yield* p1(input);
-        yield* p2(input);
-    }
+export function plus<A>(p1: Parser<A>, p2: Parser<A>): Parser<A> {
+  return function* (input: string) {
+    yield* p1(input);
+    yield* p2(input);
+  };
 }
 
-const nat = bind(many1(digit), (ds) => result(parseInt(ds.join(''))));
-const int = plus(nat, bind(seq(char('-'), nat), ([_c, n]) => result(-n)));
-const float = seq(int, seq(char('.'), nat));
+export function separatedBy<A, B>(p: Parser<A>, sep: Parser<B>): Parser<A[]> {
+  return map(seq(many(seq(p, sep)), p), (rs) =>
+    rs[0].map((r) => r[0]).concat([rs[1]])
+  );
+}
 
-let p = seq(seq(seq(many(alpha), string('aa')), many(alpha)),  seq(int, float));
-
-for(const {value, rest} of p("aaa-01234.012")) {
-    console.log({value, rest});
+export function surroundedBy<A, B, C>(
+  p: Parser<A>,
+  before: Parser<B>,
+  after: Parser<C>
+): Parser<A> {
+  return map(seq(before, seq(p, after)), (r) => r[1][0]);
 }
